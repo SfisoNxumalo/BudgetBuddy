be41d6b8a5b68c03a91ac1b5906c09806ae12e69
Sifiso Mawila <Sifiso@nebula.co.za>
2025-09-13 23:44:05 +0200

    Add dummy Dashboard and Ad Card

--- src/BudgetBuddyApp/budgetbuddyapp.client/src/main.tsx
+++ src/BudgetBuddyApp/budgetbuddyapp.client/src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<ViewUsersPage />} />
-            {/* <Route path="/" element={<App />} /> */}
+            <Route path="/" element={<App />} />
           </Routes>
         </BrowserRouter>
   </StrictMode>,
