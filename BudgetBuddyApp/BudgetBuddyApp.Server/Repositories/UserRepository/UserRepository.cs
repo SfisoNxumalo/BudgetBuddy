d6fc3813301fcefea3ec4379e22d74166366e24c
Sifiso Mawila <Sifiso@nebula.co.za>
2025-09-13 16:52:09 +0200

    Implement IUserRepository interface and create repository method

--- BudgetBuddyApp/BudgetBuddyApp.Server/Repositories/UserRepository/UserRepository.cs
+++ BudgetBuddyApp/BudgetBuddyApp.Server/Repositories/UserRepository/UserRepository.cs
@@ -1,7 +1,19 @@
+using BudgetBuddyApp.Server.Data;
+using BudgetBuddyApp.Server.Entities;
+using BudgetBuddyApp.Server.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace BudgetBuddyApp.Server.Repositories.UserRepository
 {
     public class UserRepository : IUserRepository
     {
-
+        private readonly BudgetBuddyContext _context;
+        public UserRepository(BudgetBuddyContext context) {
+            _context = context;
+        }
+        public async Task<List<UserEntity>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
     }
 }
